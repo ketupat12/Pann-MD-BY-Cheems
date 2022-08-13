8a4033189f2e511e1b13b9c3e3d97cada3ece23a
ketupat12 <111193907+ketupat12@users.noreply.github.com>
2022-08-14 00:07:29 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['082284661772'] //ur owner number
+global.ownername = "🦄KINTO BOT" //ur owner name
+global.ytname = "YT: RamadhaniR5" //ur yt chanel name
+global.socialm = "GitHub: KINTO BOT" //ur github or insta name
+global.location = "Indonesia, Pekanbaru, Belilas" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['082284661772']
+global.ownertag = '082284661772' //ur tag number
+global.botname = 'KINTO BOT' //ur bot name
+global.linkz = "https://www.instagram.com/bcrstore2022/?hl=en" //your theme url which will be displayed on whatsapp
+global.websitex = "https://https://www.instagram.com/bcrstore2022/?hl=en" //ur website to be displayed
+global.botscript = 'https://www.instagram.com/bcrstore2022/?hl=en' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "KINTO Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "🦄KINTO BOT" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
